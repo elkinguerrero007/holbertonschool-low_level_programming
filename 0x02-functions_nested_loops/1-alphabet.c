8967935df3bb96706786ebbccb900b2f36e5f114
elkinguerrero007 <3439@holbertonschool.com>
2021-10-04 18:02:00 +0000

    low_level_programming

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - print_aphabet
+ * Return: (0) Success Always
+ */
+void print_alphabet(void)
+{
+char i;
+for(i = 'a'; i < 'z'; i++)
+{  
+_putchar(i);
+}
+_putchar('\n'); 
+}
