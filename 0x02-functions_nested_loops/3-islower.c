d4b11d7dec2b83b9fc0f24d088171b27d9030684
elkinguerrero007 <3439@holbertonschool.com>
2021-10-04 22:09:49 +0000

    low_level_programming

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Validate if n is positive, negative or zero
+ * @c: character
+ * Return: (0) Success Always
+ */
+int _islower(int c)
+{
+  if (c >= 'a' && c <= 'z')
+    return (1);
+  else
+    return (0);
+}
+
+
+     
+ 
