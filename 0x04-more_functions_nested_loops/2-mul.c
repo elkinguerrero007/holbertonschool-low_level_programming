b450c098e020189b5fc5697a7544ec203ba345fd
elkinguerrero007 <3439@holbertonschool.com>
2021-10-09 20:37:54 +0000

    low_level_programming

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *mul - mul bettwen two numbers
+ *@a:back number mul
+ *@b:back number mul
+ *Return:result between a and b
+ */
+int mul(int a, int b)
+{
+int r = (a * b);
+return (r);
+}
