86bf87caa45329ea56a81a595580e22895b4ad48
elkinguerrero007 <3439@holbertonschool.com>
2021-10-09 17:20:37 +0000

    low_level_programming

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,12 @@
 #include"main.h"
 /**
  *_isdigit-veryfy is digit
- *
+ *@c:come_back_digit
  *Return 1 if digit,0 if otherwise
  */
 int _isdigit(int c)
 {
-if(c >= 48 && c <= 57)
-return(1);
-return(0);
+if (c >= 48 && c <= 57)
+return (1);
+return (0);
 }
-
-
