f9cab22a507428d7d1ef2891b41a04281df9afa3
elkinguerrero007 <3439@holbertonschool.com>
2021-10-10 04:31:57 +0000

    low_level_programming

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal
+ * @n: number of lines in diagonal
+ */
+void print_diagonal(int n)
+{
+int i, j;
+if (n < 1)
+{
+_putchar('\n');
+return;
+}
+for (i = 0; i < n ; i++)
+{
+for (j = 0; j <= i; j++)
+{
+if (j != 0 && i != 0)
+_putchar(' ');
+if (j == i)
+_putchar('\\');
+}
+_putchar('\n');
+}
+}
+
