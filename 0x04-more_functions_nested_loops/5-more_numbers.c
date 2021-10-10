cb843b2d2f5486fd07e9851b017ad7df00ba2220
elkinguerrero007 <3439@holbertonschool.com>
2021-10-10 03:47:44 +0000

    low_level_programming

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,18 +3,16 @@
  * more_numbers - Prints num 0 to 14
  *
  */
-
 void more_numbers(void)
 {
-	int a;
-
-	for (a = 0; a <= 14; a++)
-	{
-		if (a > 9)
-		{
-			_putchar(a / 10 + '0');
-		}
-		_putchar(a % 10 + '0');
-	}
-	_putchar('\n');
+int a;
+for (a = 0; a <= 14; a++)
+{
+if (a > 9)
+{
+_putchar(a / 10 + '0');
+}
+_putchar(a % 10 + '0');
+}
+_putchar('\n');
 }
