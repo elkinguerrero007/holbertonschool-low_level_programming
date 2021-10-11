56be2d2caf4c89760b43aafcdb3131cfaa543a0a
elkinguerrero007 <3439@holbertonschool.com>
2021-10-11 21:52:02 +0000

    low_level_programming

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,5 @@
 /**
- * reset_to_98 -  function that takes a pointer to an int as parameter
- *                and updates the value it points to to 98
+ * reset_to_98 -update  and updates the value it points to to 98
  * @n: int *n
  */
 void reset_to_98(int *n)
