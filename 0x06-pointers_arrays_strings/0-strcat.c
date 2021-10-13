8ab6034ae56c89aa6f62e55d377baa0941ab323a
elkinguerrero007 <3439@holbertonschool.com>
2021-10-13 22:30:03 +0000

    low_level_programming

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,9 @@
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+    return (0);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+
+#include <unistd.h>
+
+int _putchar(char c)
+char *_strcat(char *, char *);
+
+#endif
