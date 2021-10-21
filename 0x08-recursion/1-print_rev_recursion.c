2e0b781f8aadfe4484b87293fbe07ae7c4711c17
elkinguerrero007 <3439@holbertonschool.com>
2021-10-21 01:26:44 +0000

    low_level_programming

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - reverse string print
+ * @s: print s string reverse
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+
+}
