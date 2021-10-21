2e1c9ded5a513e313c3d4d91c1442e1fecda52e2
elkinguerrero007 <3439@holbertonschool.com>
2021-10-21 04:09:25 +0000

    low_level_programming

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+/**
+* _sqrt - compute square root of n number
+* @n: number to compute square
+* @i: iterator to search if is divisible by i and result is i and mod is 0
+* Return: square of -1 if not have square exact
+*/
+int _sqrt(int n, int i)
+{
+	if (n == i) 
+		return (-1);
+	if (n / i == i && n % i == 0) 
+	   
+		return (i);
+	return (_sqrt(n, ++i));
+}
+/**
+ * _sqrt_recursion - compute sqrt of n with recursion
+ * @n: number to compute square root
+ * Return: sqrt from n number
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 1)
+		return (1);
+	return  (_sqrt(n, 2));
+}
