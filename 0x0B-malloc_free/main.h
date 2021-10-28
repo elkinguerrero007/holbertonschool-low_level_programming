f0fa166c7ac706aa6b7e9e46b3aaa3b6d05d403b
elkinguerrero007 <3439@holbertonschool.com>
2021-10-28 01:56:42 +0000

    low_level_programming

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,5 +8,5 @@ char *create_array(unsigned int size, char c);
 char *create_array(unsigned int, char);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
-
+int **alloc_grid(int width, int height);
 #endif
