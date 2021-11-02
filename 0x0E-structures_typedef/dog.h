724576cbd19f6cc271d44ea154911184701d5dbe
elkinguerrero007 <3439@holbertonschool.com>
2021-11-02 00:27:26 +0000

    low_level_programming

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,14 @@
  *Description: is a structure representation of Dog
  */
 
-typedef struct dog dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-struct dog
 {
 char *name;
 float  age;
 char *owner;
 } my_dog;
+
+typedef struct dog my_dog;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+struct dog
+
 #endif
