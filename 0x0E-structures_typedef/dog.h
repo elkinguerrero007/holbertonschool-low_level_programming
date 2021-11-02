4b4d59167aa5fab1ca19817c711d0bde52d84739
elkinguerrero007 <3439@holbertonschool.com>
2021-11-02 00:38:36 +0000

    low_level_programming

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,9 +14,9 @@ struct dog
 char *name;
 float  age;
 char *owner;
-}
+} my_dog;
 
-typedef struct dog dog_t;
+typedef struct dog my_dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 struct dog
 
