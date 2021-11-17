e7a412f3115968fdc0ce74e730f9d99b34572bb3
elkinguerrero007 <3439@holbertonschool.com>
2021-11-17 01:37:47 +0000

    low_level_programming

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -25,5 +25,5 @@ typedef struct listint_s
 /* ------------------ prototypes          */
 size_t print_listint(const listint_t *);
 size_t listint_len(const listint_t *h);
-
+listint_t *add_nodeint(listint_t **head, const int n);
 #endif
