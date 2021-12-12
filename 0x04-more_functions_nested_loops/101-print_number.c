f8a0b08594b34960c2ada11863e1a0f5fc78aece
elkinguerrero007 <3439@holbertonschool.com>
2021-12-12 04:30:08 +0000

    low_level_programming

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <limits.h>
+/**
+ * print_number - print int characters
+ * @n: number to print
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	unsigned int tmp, tens = 1;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	tmp = n;
+	if (n == INT_MIN)
+		tmp++;
+
+	while (tmp > 9)
+	{
+		tmp /= 10;
+		tens *= 10;
+	}
+	tmp = n;
+	while (tens > 0)
+	{
+		_putchar('0' + tmp / tens);
+		tmp %= tens;
+		tens /= 10;
+	}
+}
